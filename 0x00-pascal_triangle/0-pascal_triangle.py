522a7015b08059bff87a8f6d65e27dd0162d4083
Abstargo <james.iharo@gmail.com>
2024-10-06 22:11:31 +0100

    commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,15 +6,15 @@
 
 def pascal_triangle(n):
     """
-    Returns a list integers
+    Returns a list of integers
     representing the Pascal Triangle of n
-    returns empty list if n= 0
+    returns empty list if n <= 0
     """
     k = []
-    if n <= 0;
+    if n <= 0:
         return k
     k = [[1]]
-    for i in range(len(k[i - 1]) - 1):
+    for i in range(1, n):
         temp = [1]
         for j in range(len(k[i - 1]) - 1):
             curr = k[i - 1]
